8e3a9a163ff7d0341ee5eea9795fc23c967c2765
Jose Soto <jsoto7087@gmail.com>
2022-07-07 16:38:05 -0400

    Added event listener: does not work

--- index.js
+++ index.js
@@ -12,19 +12,36 @@ function renderBtn(employees) {
     console.log(employeeInfo)
     document.getElementById("p-01").appendChild(employeeInfo)
 
+    addsEventListener(employees)
 }
 
-function addEventListeners() {
 
+function addsEventListener(employees) {
     const btns = document.querySelectorAll(".infoBtn");
+    console.log(btns);
     btns.forEach(btn => btn.addEventListener('click', function () {
-        alert('does this work')
+        console.log(`${employees.name}`)
     }))
-    console.log(btns)
-}
-addEventListeners()
 
-{
-    /* <p>${employees.title}</p>
-    <p>${employees.schedule}</p> */
 }
+
+// function addEventListeners() {
+
+//     const btns = document.querySelectorAll(".infoBtn");
+//     btns.addEventListeners('click', (e) => {
+//         console.log(e)
+//     })
+
+// const btns = document.querySelectorAll(".infoBtn");
+// for (let i = 0; i < btns.length; i++) {
+//     btns.forEach(btn => btn.addEventListener('click', function () {
+//         console.log('does this work')
+//     }, ))
+
+// }
+// }
+// btns.forEach(btn => btn.addEventListener('click', function () {
+//     console.log('does this work')
+// }, {
+//     once: true
+// }))
