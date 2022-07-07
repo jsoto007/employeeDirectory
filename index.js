f469adae97befe065e12681cefeb4e9e28f3347f
Jose Soto <jsoto7087@gmail.com>
2022-07-07 15:07:58 -0400

    added BTN

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 fetch('http://localhost:3000/employees')
     .then(response => response.json())
-    .then(data => {
-        return console.log(data)
-    })
+    .then(employeeData => employeeData.forEach(employee => renderData(employee)))
+
+function renderData(employees) {
+    let employeeInfo = document.createElement('li');
+    employeeInfo.className = 'infoBox'
+    employeeInfo.innerHTML = `
+    <button class="infoBtn">${employees.name}</button><br>
+    <p>${employees.title}</p>
+    <p>${employees.schedule}</p>
+`
+    console.log(employeeInfo)
+}
